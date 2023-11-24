4b2ff0d862451ffd7943a2ea5625effa404f6ced
rocuha09 <ro.cunha000@gmail.com>
2023-11-24 15:23:02 -0300

    primeiro commit inicio de projeto consumindo api chat-gpt

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+import requests
+import json
+from properties import API_KEY
+from properties import URL_MODELS
+from properties import ID_MODEL
+from properties import URL_CHAT
+
+if len(sys.argv) < 2 or len(sys.argv) > 2:
+    print("Usage: python main.py <message>")
+    exit(1)
+else:
+    message = sys.argv[1]
+
+
+''' para realizar requisição simples e receber texto sem envio de mensagem 
+headers = {
+    "Authorization": f"Bearer {API_KEY}"
+}
+'''
+
+headers = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+}
+
+body_message = {
+    "model": ID_MODEL,
+    "messages": [{
+        "role": "user",
+        "content": f"{message}clear"
+        }]
+}
+
+body_message = json.dumps(body_message)
+
+#lendo modelos disponíveis
+#req = requests.get(URL_MODELS, headers=headers)
+
+req = requests.post(URL_CHAT, headers=headers, data=body_message)
+res = req.json()
+
+message = res['choices'][0]['message']['content']
+
+print(message)
+
---
+++ properties.py
@@ -0,0 +1,4 @@
+API_KEY = "sua chave aqui"
+URL_MODELS = "https://api.openai.com/v1/models"
+URL_CHAT = "https://api.openai.com/v1/chat/completions"
+ID_MODEL = "gpt-3.5-turbo"
